bf3f59da4b0d04f37186ce7a6936078b34734078
OxFernando <oxFernando@proton.me>
2023-06-29 13:03:41 +0800

    :heavy_minus_sign: Remove some dependencies

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { Buffer } from 'buffer';
 
 import { BrowserRouter } from "react-router-dom";
-import { Web3Modal } from '@web3modal/react';
 import { Provider } from "mobx-react";
-import { WagmiConfig } from 'wagmi';
-import { WalletConnectModalAuth } from '@walletconnect/modal-auth-react';
 
-import WalletConfig from 'src/wallet-v2';
 import Store from "src/store";
 import App from "src/App";
 
@@ -16,37 +11,18 @@ import "./index.css";
 
 // @ts-ignore
 // just for walletConnect wallet v1 version
-window.Buffer = window.Buffer || Buffer;
+// window.Buffer = window.Buffer || Buffer;
 
 const root = ReactDOM.createRoot(
     document.getElementById("root") as HTMLElement
 );
 
-const MetadataConfig = {
-    name: 'ERD',
-    description: 'Ethereum reserve dollar',
-    url: 'https://app.erd.xyz',
-    icons: ['https://erd-fe-storage.s3.amazonaws.com/erd.svg']
-};
-
-function WalletComponent() {
-    return (
-        <>
-            <BrowserRouter>
-                <Provider store={new Store()}>
-                    <App />
-                </Provider>
-            </BrowserRouter>
-            <WalletConnectModalAuth
-                projectId={WalletConfig.projectId}
-                metadata={MetadataConfig}
-            />
-        </>
-    );
-}
-
 root.render(
     <React.StrictMode>
-        <WalletComponent />
+        <BrowserRouter>
+            <Provider store={new Store()}>
+                <App />
+            </Provider>
+        </BrowserRouter>
     </React.StrictMode>
 );
