9bf3e04e914cf58d452813f3b99b13bb82aebfc4
matt <oxFernando@proton.me>
2023-08-30 12:45:23 +0800

    :lipstick: Update ui

--- src/MobileAvatarMenu.tsx
+++ src/MobileAvatarMenu.tsx
@@ -38,7 +38,7 @@ export default observer(function MobileAvatarMenu() {
         >
             <div className={s.accountInfo}>
                 <div className={s.accountInfoItem}>
-                    <span>My Score</span>
+                    <span>My Points</span>
                     <p>{userScores}</p>
                 </div>
                 <div className={s.accountInfoItem}>
